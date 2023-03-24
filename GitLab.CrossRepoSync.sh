f667c9db6883f72735ce911153b052cfd33f6493
octt <6083316-octospacc@users.noreply.gitlab.com>
2023-03-24 14:30:45 +0000

    Update 3 files
    - /.gitlab-ci.yml
    - /GitLab.Main.sh
    - /GitLab.CrossRepoSync.sh

--- GitLab.CrossRepoSync.sh
+++ GitLab.CrossRepoSync.sh
@@ -2,14 +2,14 @@
 TargetRepo="OcttKB"
 git clone --depth 1 https://gitlab.com/octtspacc/$TargetRepo ./Repo-$TargetRepo
 cd ./Repo-$TargetRepo
-rm -rf ./Wiki ./Wiki-OcttKB
+rm -rf ./Wiki-OcttKB
 tiddlywiki \
 	--verbose \
 	--load ../public/index.html \
 	--output ./Wiki-OcttKB \
 	--savewikifolder ./Wiki-OcttKB
 cd ./Wiki-OcttKB/tiddlers
-mkdir -p ../Normal ../System
+mkdir -vp ../Normal ../System
 mv \$__* ../System/
 mv * ../Normal/
 mv ../System ../Normal ./
